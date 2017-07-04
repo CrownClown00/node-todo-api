d2164b3e0af267f32ed133d01875e1a130d2ea88
CrownClown00 <rossca.thompson@gmail.com>
2017-07-04 14:54:27 -0400

    added lodash depedencie and PATCH route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -68,7 +68,35 @@ app.delete('/todos/:id',(request,response)=>{
       }).catch((error)=>{
           response.status(400).send();
       })
+});
+
+app.patch('/todos/:id',(request,response)=>{
+  var id = request.params.id;
+  var body = _.pick(request.body, ['text','completed']);
+
+  if(!ObjectID.isValid(id)){
+    return response.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed)&& body.completed){
+      body.completedAt = new Date().getTime();
+  } else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+    if(!todo){
+       return response.status(404).send();
+    }
+    response.send({todo});
+  }).catch((error)=>{
+    response.status(400).send();
+  })
+
 })
+
+
 
 
 
